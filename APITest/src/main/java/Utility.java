9a92ff5cd2209924546ba4dc684de4e3c503d735
Venkat <vparine@salesforce.com>
2020-05-02 23:06:13 +0530

    Including 2 Test Cases

--- APITest/src/main/java/Utility.java
+++ APITest/src/main/java/Utility.java
@@ -3,20 +3,68 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class Utility {
 
-    public static HttpResponse getRequest(String query, String sortQuery, String orderQuery) throws IOException {
+    String githubUrl = "";
+    HttpUriRequest request = null;
+    HttpResponse response = null;
+    int responseCode = 0;
+    JSONObject jsonResponse = null;
+    int totalCount = 0;
 
-        String githubUrl = "https://api.github.com/search/repositories?q=";
-        githubUrl += query + "&sort=" + sortQuery + "&order=" + orderQuery;
+    public void searchForRepositories(String query, String sortQuery, String orderQuery) {
+        githubUrl = "https://api.github.com/search/repositories?q=";
+        try{
+            githubUrl += URLEncoder.encode(query, "UTF-8") + "&sort=" + sortQuery + "&order=" + orderQuery;
+        }catch(UnsupportedEncodingException e){
+            System.out.println("Looks like encoding format is not supported. Please check and try again!");
+        }
+        System.out.println(githubUrl);
 
-        HttpUriRequest request = new HttpGet(githubUrl);
+        request = new HttpGet(githubUrl);
+        try {
+            response = HttpClientBuilder.create().build().execute(request);
+        } catch (IOException e) {
+            System.out.println("Looks like there is a problem with Connection. Please try again!");
+        }
 
-        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+        setResponseCode(response.getStatusLine().getStatusCode());
+        try {
+            setJsonResponse(new JSONObject(EntityUtils.toString(response.getEntity())));
+        } catch (IOException e) {
+            System.out.println("Looks like response is not available for converting it to String. Please check the response once!");
+        }
+        try {
+            setTotalCount(jsonResponse.getInt("total_count"));
+        } catch (JSONException e) {
+            setTotalCount(0);
+        }
+    }
 
-        return response;
+    public void setJsonResponse(JSONObject jsonResponse) {
+        this.jsonResponse = jsonResponse;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public int getTotalCount(){
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
     }
 }
--- APITest/src/test/java/GitHub.java
+++ APITest/src/test/java/GitHub.java
@@ -1,21 +1,32 @@
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class GitHub {
+
+    private Utility utility = null;
+
+    //Instantiate Utility class
+    @BeforeClass
+    public void setup() {
+        utility = new Utility();
+    }
+
+    //This test case searches for Repositories written in Java and having Java in their names. Results are sorted by No Of Stars and in Descending Order.
     @Test
-    public void searchForRepositoriesWithName_Java_WrittenIn_Java_SortByNoOf_Stars_OrderIn_Desc()
-            throws IOException {
+    public void searchForRepositoriesWithName_Java_WrittenIn_Java_SortByNoOf_Stars_OrderIn_Desc() {
+        utility.searchForRepositories("Java+language:java", "stars", "desc");
+        Assert.assertEquals(utility.getResponseCode(), 200, "Request for Repositories didn't succeed. Please check.");
+        Assert.assertNotEquals(utility.getTotalCount(), 0, "No Repositories found for given search parameters. Refine the search and try again.");
+    }
 
-        HttpResponse response = Utility.getRequest("Java+language:java", "stars", "desc");
-
-        String responseContent = EntityUtils.toString(response.getEntity());
-
-        Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
-
-        System.out.println(responseContent);
+    //This test case searches for Repositories written in Java and created after given date. Results are sorted by No Of Stars and in Descending Order.
+    @Test
+    public void searchForRepositoriesWithGiven_CreatedDate_SortByNoOf_Stars_OrderIn_Desc() {
+        utility.searchForRepositories("Java+language:java+created:>2019-01-01", "stars", "desc");
+        Assert.assertEquals(utility.getResponseCode(), 200, "Request for Repositories didn't succeed. Please check.");
+        Assert.assertNotEquals(utility.getTotalCount(), 0, "No Repositories found for given search parameters. Refine the search and try again.");
     }
 }
